9f2b0bd55c76aa589acafe22c2eb1a87180dfb88
Christian <christianrostrupnielsen@gmail.com>
2013-12-16 08:55:09 +0100

    First protoype in Unity

---
+++ The Modern Warrior/Assets/Scripts/PlayerController.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+var speed : float;
+
+function FixedUpdate(){
+	var moveHorizontal : float = Input.GetAxis("Horizontal");
+	var moveVertical : float = Input.GetAxis("Vertical");
+	
+	var movement : Vector3 = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	rigidbody.AddForce(movement * speed * Time.deltaTime);
+}
